876d39ae4c797c4ab0377406baa36cb5cdc7f69e
wang <82146426+wzop@users.noreply.github.com>
2024-07-30 00:48:10 +0800

    Add files via upload

---
+++ scripts/setMessage.js
@@ -0,0 +1,47 @@
+const hre = require("hardhat");
+const { encryptDataField, decryptNodeResponse } = require("@swisstronik/utils");
+
+/**
+ * // 让我们sendShieldedTransaction向区块链发送调用/查询
+ * Send a shielded transaction to the Swisstronik blockchain.
+ * @param {object} signer - The signer object for sending the transaction.
+ * @param {string} destination - The address of the contract to interact with.
+ * @param {string} data - Encoded data for the transaction.
+ * @param {number} value - Amount of value to send with the transaction.
+ * @returns {Promise} - The transaction object.
+ */
+const sendShieldedTransaction = async (signer, destination, data, value) => {
+  // Get the RPC link from the network configuration
+  const rpclink = hre.network.config.url;
+  // Encrypt transaction data
+  const [encryptedData] = await encryptDataField(rpclink, data);
+  // Construct and sign transaction with encrypted data
+  return await signer.sendTransaction({
+    from: signer.address,
+    to: destination,
+    data: encryptedData,
+    value,
+  });
+};
+
+// 编写main脚本以使用sendShieldedTransaction函数
+async function main() {
+  // Address of the deployed contract
+  const contractAddress = "0xf84Df872D385997aBc28E3f07A2E3cd707c9698a";
+  // Get the signer (your account)
+  const [signer] = await hre.ethers.getSigners();
+  // Construct a contract instance
+  const contractFactory = await hre.ethers.getContractFactory("Swisstronik");
+  const contract = contractFactory.attach(contractAddress);
+  // Send a shielded transaction to set a message in the contract
+  const setMessageTx = await sendShieldedTransaction(signer, contractAddress, contract.interface.encodeFunctionData("setMessage", ["Hello Swisstronik!!"]), 0);
+  await setMessageTx.wait();
+  //It should return a TransactionResponse object
+  console.log("Transaction Receipt: ", setMessageTx);
+}
+
+// 安全帽默认添加此项
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
